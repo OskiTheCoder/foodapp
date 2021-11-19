08e4ba800bccf4283d4fdbc6a365d02011496e63
shelvy <sks91@berkeley.edu>
2021-11-18 19:32:01 -0800

    admin

--- functions/APIs/recipes.js
+++ functions/APIs/recipes.js
@@ -1,37 +1,25 @@
+const { db } = require('../util/admin');
+
 exports.getAllRecipes = (request, response) => {
-  let recipes = [
-    {
-      title: 'vegan chicken burrito',
-      description:
-        'delicious, simple, and highly customizable recipe for a vegan chicken burrito that you will love',
-      ingredients: [
-        'tortilla',
-        'vegan chicken',
-        'beans',
-        'rice',
-        'tomato',
-        'peppers',
-      ],
-      steps: [
-        'get tortilla',
-        'put everything inside tortilla',
-        'eat everything',
-      ],
-      costtomake: '6.25',
-      cooktime: '30 min',
-    },
-    {
-      title: 'pasta',
-      description: 'extremely quick and delicious pasta',
-      ingredients: ['pasta', 'pasta sauce', 'spices'],
-      steps: [
-        'boil pasta',
-        'heat up sauce',
-        'combine pasta with sauce and add spices',
-      ],
-      costtomake: '3.25',
-      cooktime: '25 min',
-    },
-  ];
-  return response.json(recipes);
+  db.collection('recipes')
+    .get()
+    .then((data) => {
+      let recipes = [];
+      data.forEach((doc) => {
+        recipes.push({
+          recipeId: doc.id,
+          title: doc.data().title,
+          cooktime: doc.data().cooktime,
+          costtomake: doc.data().costtomake,
+          description: doc.data().description,
+          ingredients: doc.data().ingredients,
+          steps: doc.data().steps,
+        });
+      });
+      return response.json(recipes);
+    })
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({ error: err.code });
+    });
 };
